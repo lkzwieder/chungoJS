3a1140c9b6f6ff8935a44405ee737dc89f8a8bc0
lkzwieder <lucas.tettamanti@gmail.com>
2016-08-24 12:39:56 -0300

    Clusterization

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
+const cluster = require('cluster');
+const http = require('http');
+const qs = require('querystring');
+const url = require('url');
+
 var config = require('./config/dev.js');
 config.routes = require('./config/routes.js');
-var http = require('http');
-var qs = require('querystring');
-var url = require('url');
-//var Router = new (require('Fwk/Router.js'))(config.routes);
 
-http.createServer(function(req, res) {
-   var body = [];
-   console.log(req);
-   req.on('error', onConnError);
-   req.on('data', onConnData);
-   req.on('end', onConnEnd);
+if(cluster.isMaster) {
+   var numReqs = 0;
+   setInterval(() => {
+      console.log('numReqs =', numReqs);
+   }, 5000);
 
-   function onConnError(err) {
-      console.log('Connection error: ', err);
-   }
-
-   function onConnData(d) {
-      body.push(d);
-   }
-
-   function onConnEnd() {
-      body = Buffer.concat(body).toString();
-      var params = req.method == 'POST' ? qs.parse(body) : url.parse(req.url, true).query;
-
-      res.on('error', function(err) {
-         console.error(err);
+   const numCPUs = require('os').cpus().length;
+   for(let i = numCPUs; i--;) cluster.fork();
+   Object.keys(cluster.workers).forEach((id) => {
+      cluster.workers[id].on('message', (msg) => {
+         if(msg.cmd && msg.cmd == 'notifyRequest') numReqs++;
       });
-   }
-}).listen(config.server.port);
+   });
+} else {
+   http.createServer((req, res) => {
+      process.send({cmd: 'notifyRequest'});
+
+      var body = [];
+      req.on('error', onConnError);
+      req.on('data', onConnData);
+      req.on('end', onConnEnd);
+
+      function onConnError(err) {
+         console.log('Connection error: ', err);
+      }
+
+      function onConnData(d) {
+         body.push(d);
+      }
+
+      function onConnEnd() {
+         body = Buffer.concat(body).toString();
+         let params = req.method == 'POST' ? qs.parse(body) : url.parse(req.url, true).query;
+
+         res.on('error', function(err) {
+            console.error(err);
+         });
+      }
+   }).listen(config.server.port);
+}
