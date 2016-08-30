e47dc8e26143743202431e516d4489153c74fa37
lkzwieder <lucas.tettamanti@gmail.com>
2016-08-30 20:26:24 -0300

    Policies almost finished

--- Fwk/Policer.js
+++ Fwk/Policer.js
@@ -1,11 +1,30 @@
 class Policer {
-   constructor(configs, policies) {
-      this._configs = configs;
+   constructor(config, policies) {
+      this._toApply = [];
       this._policies = policies;
+
+      for(let k in config) {
+         if(k == '*') {
+            if(config[k]) {
+               this._toApply = Object.keys(config);
+               this._toApply.splice(this._toApply.indexOf(k), 1);
+            } else {
+               this._toApply = [];
+            }
+         } else {
+            if(config[k]) {
+               this._toApply.push(k);
+            } else {
+               this._toApply.splice(this._toApply.indexOf(k), 1);
+            }
+         }
+      }
    }
 
    run(req, res) {
-
+      this._toApply.forEach((policy) => {
+         this._policies[policy](req, res);
+      })
    }
 }
 
